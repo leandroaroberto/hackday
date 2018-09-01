f01329d84dba3ebde86bb7b28612601170c06977
Leandro Roberto Home <leroberto@gmail.com>
2018-09-01 16:56:54 -0300

    V final

--- resources/views/tabela2.blade.php
+++ resources/views/tabela2.blade.php
@@ -12,10 +12,15 @@
       @foreach($dados as $dado)      
       <tr>
         <td>{{$dado['nome']}}</td>       
-        <td>{{$dado['status']}}</td>   
-        <td>{{$dado['consumo']}}</td>
-        <td>@if($dado['consumo'] > 1 AND $dado['consumo'] <= 100 )<span class="label label-warning">{{'Atenção'}}</span>@endif</td>  
-        <td>{{$dado['conexao']}}</td> 
+        <td>{{$dado['status']}}</td>           
+        <td>          
+            @if($dado['consumo'] <= 50 )<span class="label label-default">{{'Desligado'}}</span>@endif
+         @if($dado['consumo'] > 50 AND $dado['consumo'] <= 100 )<span class="label label-warning">{{'Atenção'}} </span>&nbsp; {{$dado['consumo'] .' Kwh' }}@endif
+          @if($dado['consumo'] > 100 AND $dado['consumo'] <= 350)<span class="label label-success">{{'Ok'}}</span>&nbsp; {{$dado['consumo'] .' Kwh' }}@endif
+          @if($dado['consumo'] > 350 AND $dado['consumo'] <= 380)<span class="label label-warning">{{'Atenção'}}</span>&nbsp; {{$dado['consumo'] .' Kwh' }}@endif
+          @if($dado['consumo'] > 380)<span class="label label-danger">{{'Alerta'}}</span>@endif  
+        </td>  
+        <td>@if($dado['conexao']==0)<span class="label label-default">{{'Inativo'}}</span>@else<span class="label label-success">{{'Ativo'}}</span>@endif</td> 
         <td>{{$dado['obs']}}</td>         
       </tr>
       
