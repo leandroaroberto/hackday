bc341c55505d044fc78992d201398c23537535b8
Ricardo Moura <contato@ricardo-moura.com>
2018-09-01 10:48:50 -0300

    Dev: Hackcontroller improvements

--- app/Http/Controllers/HackController.php
+++ app/Http/Controllers/HackController.php
@@ -9,6 +9,9 @@ use GuzzleHttp\Client;
 
 class HackController extends Controller
 {
+
+    const API_KEY = '1BkpLh7hc5O8XIEuJHMPZFnhdaG1slLu';
+
     /**
      * Display a listing of the resource.
      *
@@ -49,21 +52,40 @@ class HackController extends Controller
     }*/
 
 
-    public function getToken(){
+//    public function getToken(){
+//
+//        try {
+//            $token = null;
+//            $headers = [
+//                'Accept' => 'application/json',
+//                'Authorization' => 'Bearer ' . $token,
+//                'Content-Type' => 'application/json',
+//            ];
+//
+//
+//            $client = new Client(['base_uri' => 'https://portal.stg.eugenio.io/api/']);
+//            $res = $client->request('GET', 'v1/data/query?apikey=1BkpLh7hc5O8XIEuJHMPZFnhdaG1slLu&sql=SELECT%20%2A%20FROM%20hackday_time3_sandbox.v_teste', [
+//            ]);
+//
+//            $responde = $res->getBody();
+//            $arrayDecoded = json_decode($responde);
+//            //dd($arrayDecoded);
+//            return $arrayDecoded;
+//            //return $arrayDecoded->access_token;
+//        } catch (\Exception $e) {
+//            return $e->getMessage();
+//        }
+//    }
+
+    public function getSites()
+    {
 
         try {
-            $token = null;
-            $headers = [
-                'Accept' => 'application/json',
-                'Authorization' => 'Bearer ' . $token,
-                'Content-Type' => 'application/json',
-            ];           
-            
 
             $client = new Client(['base_uri' => 'https://portal.stg.eugenio.io/api/']);
-            $res = $client->request('GET', 'v1/data/query?apikey=1BkpLh7hc5O8XIEuJHMPZFnhdaG1slLu&sql=SELECT%20%2A%20FROM%20hackday_time3_sandbox.v_teste', [
+            $res = $client->request('GET', 'v1/data/query?apikey=' . self::API_KEY . '&sql=SELECT%20%2A%20FROM%20hackday_time3_sandbox.v_teste', [
             ]);
-            
+
             $responde = $res->getBody();
             $arrayDecoded = json_decode($responde);
             //dd($arrayDecoded);
@@ -73,6 +95,7 @@ class HackController extends Controller
             return $e->getMessage();
         }
     }
+
 
     /**
      * Show the form for creating a new resource.
@@ -87,7 +110,7 @@ class HackController extends Controller
     /**
      * Store a newly created resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
+     * @param  \Illuminate\Http\Request $request
      * @return \Illuminate\Http\Response
      */
     public function store(Request $request)
@@ -98,7 +121,7 @@ class HackController extends Controller
     /**
      * Display the specified resource.
      *
-     * @param  int  $id
+     * @param  int $id
      * @return \Illuminate\Http\Response
      */
     public function show($id)
@@ -109,7 +132,7 @@ class HackController extends Controller
     /**
      * Show the form for editing the specified resource.
      *
-     * @param  int  $id
+     * @param  int $id
      * @return \Illuminate\Http\Response
      */
     public function edit($id)
@@ -120,8 +143,8 @@ class HackController extends Controller
     /**
      * Update the specified resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
+     * @param  \Illuminate\Http\Request $request
+     * @param  int $id
      * @return \Illuminate\Http\Response
      */
     public function update(Request $request, $id)
@@ -132,7 +155,7 @@ class HackController extends Controller
     /**
      * Remove the specified resource from storage.
      *
-     * @param  int  $id
+     * @param  int $id
      * @return \Illuminate\Http\Response
      */
     public function destroy($id)
@@ -141,14 +164,15 @@ class HackController extends Controller
     }
 
 
-    public function getDataEugenio(){
+    public function getDataEugenio()
+    {
         //acessa api do eugenio
 
         //pega os dados
 
         //renderiza para a tabela
 
-        return "Dados da Api do Eugenio: ". rand(100,2);
+        return "Dados da Api do Eugenio: " . rand(100, 2);
 
     }
 
